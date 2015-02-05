626fa2d533cd94a9a292ee0c70d23ab8f1a1b417
AnurajG <leo.anuraj@gmail.com>
2015-02-05 16:58:24 +0530

    initial commit

---
+++ src/com/example/hello_world/DisplayMessageActivity.java
@@ -0,0 +1,52 @@
+package com.example.hello_world;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_display_message);
+Intent intent =getIntent();
+String msg=intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        
+        
+        TextView tv= new TextView(this);
+        tv.setTextSize(40);
+        tv.setText(msg);
+        setContentView(tv);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    /*public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() { }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                  Bundle savedInstanceState) {
+              View rootView = inflater.inflate(R.layout.fragment_display_message,
+                      container, false);
+              return rootView;
+        }
+    }*/
+}
