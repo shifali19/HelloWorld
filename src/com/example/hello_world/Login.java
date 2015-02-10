02fe3d5fa8aae724e12c5cb9480d57b893802b32
shifali19 <SCHOUBE@CGSCL5CB2246TZV.in623.corpintra.net>
2015-02-10 19:07:09 +0530

    sign up

--- src/com/example/hello_world/Login.java
+++ src/com/example/hello_world/Login.java
@@ -8,17 +8,25 @@ import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.parse.LogInCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseInstallation;
 import com.parse.ParsePush;
+import com.parse.ParseUser;
 import com.parse.SaveCallback;
+import com.parse.SignUpCallback;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -28,6 +36,8 @@ import android.widget.Toast;
 public class Login extends Activity implements OnClickListener{
 	private EditText user, pass;
 	private Button bLogin;
+	private EditText SignupUser, SignupPass;
+	private Button Signup;
 	// Progress Dialog
 	private ProgressDialog pDialog;
 	// JSON parser class
@@ -57,33 +67,79 @@ public class Login extends Activity implements OnClickListener{
 		pass = (EditText)findViewById(R.id.passwordET);          
 		bLogin = (Button)findViewById(R.id.loginBtn);
 		bLogin.setOnClickListener(this);
+		Signup = (Button)findViewById(R.id.signUpBtn);
+		Signup.setOnClickListener(this);
 	}
 
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
+		 ContentResolver cr = getContentResolver();
+		Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+                null, null, null, null);
+		if (cur.getCount() > 0) {
+			   while (cur.moveToNext()){
+				   String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+				   Log.d("DEBUG","Name : "+name);
+				   String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+				   Log.d("DEBUG","Id : "+id);
+		//Log.d("DEBUG","Number : "+cur.getString(cur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)));
+				   if (Integer.parseInt(cur.getString(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+					   Cursor pCur = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = ?",  new String[]{id}, null);
+					   String phone;
+					   while (pCur.moveToNext()) {
+						   phone = pCur .getString(pCur .getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)); 
+						   Log.d("DEBUG","Number : "+phone);
+					   }
+				   }
+			   }
+		}
 		switch (v.getId()) {
 		case R.id.loginBtn:
 			new AttemptLogin().execute();
-			// here we have used, switch case, because on login activity you may //also want to show registration button, so if the user is new ! we can go the //registration activity , other than this we could also do this without switch //case.
+			break;
 		case R.id.signUpBtn:
-			
+			new AttemptSignup().execute();
+			break;
 		default:
 			break;
 		}
 	}
 	
 	class AttemptSignup extends AsyncTask<String, String, String> {
-
 		@Override
-		protected String doInBackground(String... arg0) {
-			Intent ii = new Intent(Login.this,OtherActivity.class);
-			finish();
-			// this finish() method is used to tell android os that we are done with current //activity now! Moving to other activity
-			startActivity(ii);
-			return "Signin up.";
-		}
-		
+		protected String doInBackground(String... args) {
+				// TODO Auto-generated method stub
+				// here Check for success tag
+				int success;
+				String username = user.getText().toString();
+				String password = pass.getText().toString();
+				ParseUser user = new ParseUser();
+				user.setUsername(username);
+				user.setPassword(password);
+				TelephonyManager mTelephonyMgr;  
+				mTelephonyMgr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);   
+
+				int yourNumber = Integer.parseInt(mTelephonyMgr.getLine1Number());  
+				Log.d("DEBUG","Contact NUmber : "+yourNumber);
+				if(yourNumber<0)
+				user.put("contact_no",99999999);
+				else
+					user.put("contact_no",yourNumber);
+				user.signUpInBackground(new SignUpCallback() {
+					  public void done(ParseException e) {
+						 if (e == null) {
+					    	Intent ii = new Intent(Login.this,OtherActivity.class);
+							finish();
+							// this finish() method is used to tell android os that we are done with current //activity now! Moving to other activity
+							startActivity(ii);
+							} else {
+						Toast.makeText(Login.this, e.getMessage(), Toast.LENGTH_LONG).show();
+					    }
+					  }
+					});
+				return null;
+	   }
 	}
 
 	class AttemptLogin extends AsyncTask<String, String, String> {
@@ -101,8 +157,26 @@ public class Login extends Activity implements OnClickListener{
 			pDialog.setCancelable(true);
 			pDialog.show();
 		}
-
+		String username = user.getText().toString();
+		String password = pass.getText().toString();
+		
 		@Override
+		protected String doInBackground(String... args) {
+		ParseUser.logInInBackground(username,password, new LogInCallback() {
+			  public void done(ParseUser user, ParseException e) {
+			    if (user != null) {
+			    	Intent ii = new Intent(Login.this,OtherActivity.class);
+					finish();
+					// this finish() method is used to tell android os that we are done with current //activity now! Moving to other activity
+					startActivity(ii);
+			    } else {
+			    	Toast.makeText(Login.this, e.getMessage(), Toast.LENGTH_LONG).show();
+			    }
+			  }
+			});
+		return null;
+		}
+/*		@Override
 		protected String doInBackground(String... args) {
 			// TODO Auto-generated method stub
 			// here Check for success tag
@@ -143,7 +217,7 @@ public class Login extends Activity implements OnClickListener{
 			}
 
 			return null;
-		}
+		}*/
 		/**
 		 * Once the background process is done we need to  Dismiss the progress dialog asap
 		 * **/
